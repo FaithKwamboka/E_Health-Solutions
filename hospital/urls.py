9d593c4e4a9d24d5781973e1152a6db9c04410ab
FaithKwamboka <okongofaith3@gmail.com>
2022-06-22 11:04:59 +0300

    Feat: Add urls

--- hospital/urls.py
+++ hospital/urls.py
@@ -14,6 +14,10 @@ urlpatterns = [
     path('logout', LogoutView.as_view(template_name='index.html'),name='logout'),
     
     path('doctor-dashboard', views.doctor_dashboard,name='doctor-dashboard'),
+    path('search', views.search,name='search'),
+    
+    path('doctor-patient', views.doctor_patient,name='doctor-patient'),
+    path('doctor-view-patient', views.doctor_view_patient,name='doctor-view-patient'),
 
     
     
