aa132c2e2f1df77ba3472978c630268b6bf31918
SusanMageto <susan.mageto@student.moringaschool.com>
2022-06-22 17:37:43 +0300

    added views logic

--- hospital/views.py
+++ hospital/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse 
+from . import forms,models
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import Group
 
 # Create your views here.
 def landingpage(request):
     return render(request,'index.html')
+
+
+
+# patient view
+
+def patient_view(request):
+    userForm=forms.PatientUserForm()
+    patientForm=forms.PatientForm()
+    mydict={'userForm':userForm,'patientForm':patientForm}
+    if request.method=='POST':
+        userForm=forms.PatientUserForm(request.POST)
+        patientForm=forms.PatientForm(request.POST,request.FILES)
+        if userForm.is_valid() and patientForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            patient=patientForm.save(commit=False)
+            patient.user=user
+            patient.assignedDoctorId=request.POST.get('assignedDoctorId')
+            patient=patient.save()
+            my_patient_group = Group.objects.get_or_create(name='PATIENT')
+            my_patient_group[0].user_set.add(user)
+        return HttpResponseRedirect('patientlogin')
+    return render(request,'patient/index.html',context=mydict)
